63432a5193cab2f4b21aebde933b813120fd16b6
Tim <posseslash@gmail.com>
2023-04-06 16:44:45 +0100

    added solution for 1-module 1-task cleanup

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,13 +1,3 @@
-// function sum(a, b) {
-//   return +a + +b;
-// }
-//
-// try {
-//   sum(1, 2);
-// } catch (err) {
-//   console.log(err);
-// }
-
 function sum(a, b) {
   if (typeof a != 'number' || typeof b != 'number') {
     throw new TypeError();
